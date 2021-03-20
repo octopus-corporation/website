5ed0a6558e73b12c66c848d2f3ead70d5d45dc03
Gustavo <gustavo.bordinho@gmail.com>
2021-03-20 19:58:47 -0300

    fix typo

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <main className={styles.introContainer}>
       <div className={styles.introWrapper}>
-        <div classNAme={styles.textWrapper}>
+        <div className={styles.textWrapper}>
           <p className={styles.mainText}>A Octopus fornece os principais serviços para dar um UP na sua empresa!</p>
           <p className={styles.secondaryText}>A verdadeira mudança começa dentro de nós mesmos...</p>
         </div>
