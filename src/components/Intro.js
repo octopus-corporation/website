b04c3e814a966a90ed2f0ce00f4965a40dc25b1b
alexandreespejo <alexandreespejo98@gmail.com>
2021-03-27 14:36:29 -0300

    Feat: estilização do planeta e animação

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,11 +4,20 @@ function Header() {
   return (
     <main className={styles.introContainer}>
       <div className={styles.introWrapper}>
-        <div className={styles.textWrapper}>
-          <p className={styles.mainText}>A Octopus fornece os principais serviços para dar um UP na sua empresa!</p>
-          <p className={styles.secondaryText}>A verdadeira mudança começa dentro de nós mesmos...</p>
-        </div>
         <div className={styles.planet}>
+          <div className={styles.planetContent}></div>
+        </div>
+        <div className={styles.textWrapper}>
+          <p className={styles.mainText}>
+            A Octopus fornece os principais serviços para dar um UP na sua
+            empresa!
+          </p>
+          <p className={styles.secondaryText}>
+            A verdadeira mudança começa dentro de nós mesmos...
+          </p>
+          <button type="button" className={styles.knowMoreButton}>
+            Saiba mais
+          </button>
         </div>
       </div>
     </main>
